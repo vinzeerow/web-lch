0738a12d7fa7bfa74db086a1e95b10183f776852
vinzeerow <tpvinh2001@gmail.com>
2023-09-16 20:05:52 +0700

    Update

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,163 +1,12 @@
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Container from '@mui/material/Container';
 import styled from 'styled-components';
-import Button, { ButtonProps } from '@mui/material/Button';
-// import { SubmitHandler, useForm } from 'react-hook-form';
-// import emailjs from 'emailjs-com';
+import Button from '@mui/material/Button';
 import { PUBLIC_KEY, SERVICE_ID, TEMPLATE_ID } from '../../config';
-import { useEffect, useRef } from 'react';
-
-// interface FormData {
-//     name: string;
-//     email: string;
-//     message: string;
-// }
-
-// const FormContact: React.FC = () => {
-//     const form = useRef(null);
-
-//     const { register, handleSubmit, reset, formState: { errors } } = useForm();
-
-//     const onSubmit = () => {
-//         console.log("dkasjflksdjflkdjs", form.current);
-//         emailjs
-//             .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
-//             .then((response) => {
-//                 console.log("Sucess");
-
-//                 reset();
-//             })
-//             .catch((error) => {
-//                 console.error('Error sending email:', error);
-//             });
-//     };
-
-//     return (
-
-//         <Container sx={{ pt: 1, pb: 12 }} maxWidth="lg" ref={form} id='contact-form' onSubmit={onSubmit}>
-//             <Grid container spacing={3}>
-//                 {/* <p ref={paraRef}>Current renders: {countRenders.current}</p> */}
-//                 <Grid item xs={12} sm={6}>
-
-//                     <Typography variant="h6" gutterBottom sx={{ textTransform: "uppercase", py: "1rem" }}>
-//                         Liên hệ với LCHSV Trà Vinh
-//                     </Typography>
-//                     <Grid container spacing={3}>
-//                         <Grid item xs={12}>
-//                             <StyledTextField
-//                                 required
-//                                 error
-//                                 helperText="Hãy nhập họ và tên"
-//                                 id="name"
-
-//                                 label="Họ và tên"
-//                                 fullWidth
-//                                 autoComplete="given-name"
-//                                 variant="outlined"
-
-//                             />
-//                         </Grid>
-//                         <Grid item xs={12} sm={6}>
-//                             <StyledTextField
-//                                 required
-//                                 id="code"
-//                                 name="code"
-//                                 label="Mã số sinh viên"
-//                                 fullWidth
-//                                 autoComplete="given-name"
-//                                 variant="outlined"
-//                             />
-//                         </Grid>
-//                         <Grid item xs={12} sm={6}>
-//                             <StyledTextField
-//                                 required
-//                                 id="course"
-//                                 name="course"
-//                                 label="Khóa"
-//                                 fullWidth
-//                                 autoComplete="family-name"
-//                                 variant="outlined"
-//                             />
-//                         </Grid>
-//                         <Grid item xs={12}>
-//                             <StyledTextField
-//                                 required
-//                                 id="major"
-//                                 name="major"
-//                                 label="Ngành học"
-//                                 fullWidth
-//                                 autoComplete="shipping address-line1"
-//                                 variant="outlined"
-//                             />
-//                         </Grid>
-//                         <Grid item xs={12}>
-//                             <StyledTextField
-//                                 required
-//                                 id="email"
-//                                 name="email"
-//                                 label="Địa chi Email sinh viên"
-//                                 fullWidth
-//                                 autoComplete="shipping address-line2"
-//                                 variant="outlined"
-//                             />
-//                         </Grid>
-//                         <Grid item xs={12}>
-//                             <StyledTextField
-//                                 id="content"
-//                                 name="content"
-//                                 label="Nội dung liên hệ"
-//                                 fullWidth
-//                                 autoComplete="shipping address-line2"
-//                                 variant="outlined"
-//                                 multiline
-//                                 rows={6}
-//                             />
-//                         </Grid>
-//                         <Grid item xs={12}
-//                             display="flex"
-//                             flexDirection="row"
-//                             justifyContent="flex-end"
-//                             alignItems="flex-end"
-//                             width="100%"
-//                         >
-//                             <Button
-//                                 onClick={onSubmit}
-//                                 sx={{
-//                                     backgroundColor: "#830000",
-//                                     '&:hover': {
-//                                         backgroundColor: '#570505',
-//                                         boxShadow: 'none',
-//                                     },
-//                                 }}
-//                                 variant="contained">
-//                                 Gửi mail
-//                             </Button>
-//                         </Grid>
-
-
-//                     </Grid>
-
-//                 </Grid>
-//                 <Grid item xs={12} sm={6}>
-
-//                 </Grid>
-//             </Grid>
-
-//         </Container>
-
-
-//     );
-// }
-// export default FormContact;
-
-
-
 import React from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import emailjs from 'emailjs-com';
 
 interface FormData {
@@ -179,7 +28,6 @@ const Form: React.FC = () => {
 
     const onSubmit = async (data: FormData) => {
         console.log("DATA", { data });
-
         emailjs.send(
             SERVICE_ID,
             TEMPLATE_ID,
@@ -189,20 +37,16 @@ const Form: React.FC = () => {
             console.log("Success", response);
 
             reset();
-        })
-            .catch((error) => {
-                console.error('Error sending email:', error);
-            });
-
+        }).catch((error) => {
+            console.error('Error sending email:', error);
+        });
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <Container sx={{ pt: 1, pb: 12 }} maxWidth="lg">
                 <Grid container spacing={3}>
-                    {/* <p ref={paraRef}>Current renders: {countRenders.current}</p> */}
                     <Grid item xs={12} sm={6}>
-
                         <Typography variant="h6" gutterBottom sx={{ textTransform: "uppercase", py: "1rem" }}>
                             Liên hệ với LCHSV Trà Vinh
                         </Typography>
@@ -210,23 +54,19 @@ const Form: React.FC = () => {
                             <Grid item xs={12}>
                                 <StyledTextField
                                     required
-
                                     id="name"
-
                                     label="Họ và tên"
                                     fullWidth
                                     autoComplete="given-name"
                                     variant="outlined"
                                     error={!!errors.name}
                                     {...register('name', { required: true })}
-
                                 />
                             </Grid>
                             <Grid item xs={12} sm={6}>
                                 <StyledTextField
                                     required
                                     id="code"
-
                                     label="Mã số sinh viên"
                                     fullWidth
                                     autoComplete="given-name"
@@ -239,7 +79,6 @@ const Form: React.FC = () => {
                                 <StyledTextField
                                     required
                                     id="course"
-
                                     label="Khóa"
                                     fullWidth
                                     autoComplete="family-name"
@@ -252,7 +91,6 @@ const Form: React.FC = () => {
                                 <StyledTextField
                                     required
                                     id="major"
-
                                     label="Ngành học"
                                     fullWidth
                                     autoComplete="shipping address-line1"
@@ -265,7 +103,6 @@ const Form: React.FC = () => {
                                 <StyledTextField
                                     required
                                     id="email"
-
                                     label="Địa chi Email sinh viên"
                                     fullWidth
                                     autoComplete="shipping address-line2"
@@ -277,7 +114,6 @@ const Form: React.FC = () => {
                             <Grid item xs={12}>
                                 <StyledTextField
                                     id="content"
-
                                     label="Nội dung liên hệ"
                                     fullWidth
                                     autoComplete="shipping address-line2"
@@ -308,44 +144,13 @@ const Form: React.FC = () => {
                                     Gửi mail
                                 </Button>
                             </Grid>
-
-
                         </Grid>
-
                     </Grid>
                     <Grid item xs={12} sm={6}>
 
                     </Grid>
                 </Grid>
-
             </Container>
-            {/* <Typography variant="h4">Contact Us</Typography>
-      <TextField
-        {...register('name', { required: true })}
-        label="Name"
-        fullWidth
-        error={!!errors.name}
-        helperText={errors.name ? 'Name is required' : ''}
-      />
-      <TextField
-        {...register('email', { required: true })}
-        label="Email"
-        fullWidth
-        error={!!errors.email}
-        helperText={errors.email ? 'Email is required' : ''}
-      />
-      <TextField
-        {...register('content', { required: true })}
-        label="Message"
-        multiline
-        rows={4}
-        fullWidth
-        error={!!errors.content}
-        helperText={errors.content ? 'Message is required' : ''}
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Send
-      </Button> */}
         </form>
     );
 };
@@ -365,7 +170,6 @@ const StyledTextField = styled(TextField)`
       cursor: pointer;
     }
     &.Mui-focused fieldset {
-
       border:1px solid #2f3842;
     }
   }
